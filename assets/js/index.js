ef1eb9907aba9d98eccb6858aa66ce0d4f3b2be8
Daniel GB <danielgb_80@hotmail.com>
2023-09-30 12:27:41 -0600

    piezas de ajedrez

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
 console.log("Entro index.js");
+console.log(piezas);
+
+
+const inputBusqueda = document.getElementById("inputBusqueda");
+const resultadoBusqueda = document.getElementById("resultadoBusqueda");
+const imgPieza = document.getElementById("imgPieza");
+
+function buscar(){
+    console.log("Entro a función buscar");
+}
